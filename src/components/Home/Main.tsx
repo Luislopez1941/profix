068618562b3831bcefb9973f9d1ce44a1033a880
Luislopez1941 <jorg3luislopezc1941@hotmail.com>
2024-10-05 21:03:35 -0500

    second commit

--- src/components/Home/Main.tsx
+++ src/components/Home/Main.tsx
@@ -4,6 +4,17 @@ import React from 'react';
 import './Main.css';
 import items from './json/items.json';
 
+interface Skill {
+    name: string;
+}
+
+interface Item {
+    image: string;
+    fullname: string;
+    skills: Skill[];
+    starts: number[]; // Asegúrate de que esto sea el tipo correcto
+    // name: string; // Elimina esta línea si no es necesaria
+}
 
 const Main = () => {
     return (
@@ -11,10 +22,10 @@ const Main = () => {
             <div className='row__four'>
                 <div>
                     <div className='left'>
-                        Recuerda que puedes calificar a profecionales para reconocer su trabajo
+                        Recuerda que puedes calificar a profesionales para reconocer su trabajo
                     </div>
                     <div className='right'>
-                        Una buenas calificacion habla de un buen servicio
+                        Una buena calificación habla de un buen servicio
                     </div>
                 </div>
             </div>
@@ -23,26 +34,38 @@ const Main = () => {
                     <p>Los trabajadores con mejores calificaciones</p>
                 </div>
                 <div className='best__services'>
-                    {items.map((x: any, index: any) => (
+                    {items.map((item: Item, index: number) => (
                         <div className='item' key={index}>
-                            <div className='image__container' style={{ backgroundImage: `url(${x.image})` }} />
+                            <div 
+                                className='image__container' 
+                                style={{ backgroundImage: `url(${item.image})` }} 
+                            />
                             <div className='content__data'>
-                                <p>{x.fullname}</p>
+                                <p>{item.fullname}</p>
                                 <div className='skills'>
-                                    {x.skills.map((x: any) => (
-                                        <p className={x.name}>{x.name}</p>
+                                    {item.skills.map((skill: Skill, skillIndex: number) => (
+                                        <p className={skill.name} key={skillIndex}>
+                                            {skill.name}
+                                        </p>
                                     ))}
                                 </div>
                                 <div>
-                                    {x.starts.map((_: any) => (
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-star">
+                                    {item.starts.map((_, starIndex: number) => (
+                                        <svg 
+                                            key={starIndex} 
+                                            xmlns="http://www.w3.org/2000/svg" 
+                                            width="20" 
+                                            height="20" 
+                                            viewBox="0 0 24 24" 
+                                            fill="currentColor" 
+                                            className="icon icon-tabler icons-tabler-filled icon-tabler-star"
+                                        >
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                             <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
                                         </svg>
                                     ))}
-
                                 </div>
-                                <p>{x.name}</p>
+                       
                             </div>
                             <div className='btn'>
                                 <button>Ver perfil</button>
@@ -53,9 +76,7 @@ const Main = () => {
             </div>
             <div className='row__two'>
                 <div>
-                    <div className='left'>
-
-                    </div>
+                    <div className='left'></div>
                     <div className='right'>
                         <h2>¡Descubre la Excelencia en Servicios!</h2>
                         <p>En nuestro sitio, te ofrecemos un mundo de posibilidades para todas tus necesidades.</p>
@@ -68,9 +89,7 @@ const Main = () => {
                 </div>
             </div>
             <div className='row__three'>
-                <div>
-
-                </div>
+                <div></div>
             </div>
         </div>
     );
