b558b6f41803c36d8f87235181c4bc06a8b7dde3
Luislopez1941 <jorg3luislopezc1941@hotmail.com>
2024-10-07 16:23:49 -0500

    two

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import Link from 'next/link';
 
 const Header = () => {
+
+  const [toggle, setToggle] = useState<boolean>(false)
+
   return (
     <div className='hero'>
       <div className='hero__container'>
+        <div className='toggle' onClick={() => setToggle(!toggle)}>
+          <button className={`toggle__botton ${toggle ? 'activo' : ''}`}>
+            <span className="l1 span"></span>
+            <span className="l2 span"></span>
+            <span className="l3 span"></span>
+          </button>
+        </div>
         <div>
           <h2 className='title'>ProFix</h2>
         </div>
@@ -17,12 +27,12 @@ const Header = () => {
               Unirme
             </Link>
           </div>
-          <div className='login__hero'>
-            <Link href='/login' className='text__login'>Iniciar sesión</Link>
+          <Link href='/login' className='login__hero'>
+            <p className='text__login'>Iniciar sesión</p>
             <div className='icon__user'>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#fff" className="icon icon-tabler icons-tabler-filled icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" /><path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" /></svg>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
