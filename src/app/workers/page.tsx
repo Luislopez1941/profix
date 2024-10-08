d39a7649e6ed5e8208d19da1cd2e5175c09a1204
Luislopez1941 <jorg3luislopezc1941@hotmail.com>
2024-10-08 11:31:06 -0500

    fix id

--- src/app/workers/page.tsx
+++ src/app/workers/page.tsx
@@ -131,8 +131,8 @@ const Page = () => {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg>
           </div>
           <div className='row__pages_container'>
-            {pages.map((x: number) => (
-              <div>{x}</div>
+            {pages.map((x: number, index: number) => (
+              <div key={index}>{x}</div>
             ))}
           </div>
           <div className='chevron-right'>
